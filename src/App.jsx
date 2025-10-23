d57c35d9e47bce8b9539b946019fff7c3d59b20d
ruchithavemula <2300032021@kluniversity.in>
2025-10-23 21:05:50 +0530

    react code

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './style.css'
+
+function App() {
+  
+
+  return (
+    <div>
+      <h>Welcome to lab3</h>
+      <p>Git actions</p>
+      <p>This is ruchitha</p>
+    </div>
+  );
+}
+
+export default App
